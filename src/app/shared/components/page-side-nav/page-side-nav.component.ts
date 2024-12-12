6f99cdd2d28e1ad6b0955f14856f920593e318e4
HakeemUT01184 <hakeemmass1999@gmail.com>
2024-12-12 23:10:14 +0530

    small change id

--- src/app/shared/components/page-side-nav/page-side-nav.component.ts
+++ src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -6,6 +6,7 @@ import { UserType } from '../../../models/models';
 export interface NavigationItem {
   value: string;
   link: string;
+  icon: string; // New icon property
 }
 
 @Component({
@@ -17,37 +18,37 @@ export class PageSideNavComponent {
   panelName: string = '';
   navItems: NavigationItem[] = [];
 
-  loggedNav:boolean=true;
+  loggedNav: boolean = true;
 
   constructor(private apiService: ApiService, private router: Router) {
     apiService.userStatus.subscribe({
       next: (status) => {
         if (status == 'loggedIn') {
-          this.loggedNav=true;
+          this.loggedNav = true;
           router.navigateByUrl('/home');
           let user = apiService.getUserInfo();
           if (user != null) {
             if (user.userType == UserType.ADMIN) {
               this.panelName = 'Admin Panel';
               this.navItems = [
-                { value: 'View Books', link: '/home' },
-                { value: 'Maintenance', link: '/maintenance' },
-                { value: 'Return Book', link: '/return-book' },
-                { value: 'View Users', link: '/view-users' },
-                { value: 'Aprooval Requests', link: '/approval-requests' },
-                { value: 'All Orders', link: '/all-orders' },
-                { value: 'My Orders', link: '/my-orders' },
+                { value: 'View Books', link: '/home',icon: 'bi-box' },
+                { value: 'Maintenance', link: '/maintenance', icon: 'bi-gear' },
+                { value: 'Return Book', link: '/return-book', icon: 'bi-arrow-repeat' },
+                { value: 'View Users', link: '/view-users', icon: 'bi-person-lines-fill' },
+                { value: 'Approval Requests', link: '/approval-requests', icon: 'bi-file-earmark-check' },
+                { value: 'All Orders', link: '/all-orders', icon: 'bi-box' },
+                { value: 'My Orders', link: '/my-orders', icon: 'bi-box-arrow-in-right' },
               ];
             } else if (user.userType == UserType.STUDENT) {
               this.panelName = 'Student Panel';
               this.navItems = [
-                { value: 'View Books', link: '/home' },
-                { value: 'My Orders', link: '/my-orders' },
+                { value: 'View Books', link: '/home', icon: 'bi-house-door' },
+                { value: 'My Orders', link: '/my-orders', icon: 'bi-box-arrow-in-right' },
               ];
             }
           }
         } else if (status == 'loggedOff') {
-          this.loggedNav=false;
+          this.loggedNav = false;
           this.panelName = 'Auth Panel';
           router.navigateByUrl('/login');
           this.navItems = [];
